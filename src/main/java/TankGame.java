bd310e229806124f4f39ca135fad97174c6bae8f
master Paul <qazdse123@gmail.com>
2020-12-19 14:23:55 +0800

    按鍵偵測(進行坦克移動)

--- src/main/java/TankGame.java
+++ src/main/java/TankGame.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class TankGame {
     public static void main(String[] args) {
@@ -10,10 +12,25 @@ public class TankGame {
         frame.add(gameClient);
         frame.pack();
 
+        //置中顯示
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         gameClient.repaint();
+
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                System.out.println((char) e.getKeyCode());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+            }
+        });
     }
 }
